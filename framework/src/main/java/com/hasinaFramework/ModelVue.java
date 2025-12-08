2805938e44b2a70f6ef161ec339b923408b2c9c9
Hasina-Princy21 <hasinarandria18@gmail.com>
2025-12-08 08:44:44 +0200

    ModelVue
    creation class

--- framework/src/main/java/com/hasinaFramework/ModelVue.java
+++ framework/src/main/java/com/hasinaFramework/ModelVue.java
@@ -1,5 +1,38 @@
 package com.hasinaFramework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModelVue {
-    
+        private String vue;
+    private Map<String, Object> data;
+
+    public ModelVue(String vue) {
+        this.vue = vue;
+        this.data = new HashMap<>();
+    }
+
+    public String getPath(){
+        return "WEB-INF/views/";
+    }
+
+    public String getVue() {
+        return this.getPath() + vue;
+    }
+
+    public void setVue(String vue) {
+        this.vue = vue;
+    }
+
+    public Map<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(Map<String, Object> data) {
+        this.data = data;
+    }
+
+    public void addData(String key, Object value){
+        this.data.put(key, value);
+    }
 }
